0b2ce12eb86cfe8cca2d924472e118827a02bba8
forevertyler <1318719240@qq.com>
2016-10-11 16:23:49 +0800

    a

--- client/src/components/ui/EditForm.js
+++ client/src/components/ui/EditForm.js
@@ -4,23 +4,40 @@ export default class EditForm extends React.Component {
   handleSubmit(e) {
     e.preventDefault();
     console.log('submit');
+    const title= this.refs.title.value;
+    const content= this.refs.content.value;
+    const category= this.refs.category.value;
+    // {title:"hello", content: "my content", category: "tig"}
+    // var dataObject = {
+    //   title: title,
+    //   content: content,
+    //   category: category
+    // }
+    // // http://es6.ruanyifeng.com/#docs/object#属性的简洁表示法
+    //
+    // var dataObject = {
+    //   title,
+    //   content,
+    //   category
+    // }
+    this.publishPost({title, content, category});
+    //http://babeljs.cn/repl/# you can try es6 Here
   }
    render() {
-     console.log(this.props);
      return(
        <div>
-         <form onSubmit={this.handleSubmit}>
+         <form onSubmit={this.handleSubmit.bind(this)}>
           <div>
             <label>title</label>
-            <input defaultValue={this.props.post.title} />
+            <input ref="title" defaultValue={this.props.post.title} />
           </div>
            <div>
              <label>content</label>
-             <textarea defaultValue={this.props.post.content} />
+             <textarea ref="content" defaultValue={this.props.post.content} />
            </div>
           <div>
             <label>category</label>
-            <input defaultValue={this.props.post.category} />
+            <input ref="category" defaultValue={this.props.post.category} />
           </div>
            <input type="submit" />
          </form>
